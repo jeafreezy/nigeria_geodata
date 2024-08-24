9ce6c66c4cf8f9fc921593c85e9117dfea7b6caa
jeafreezy <jolaiyaemmanuel@gmail.com>
2024-08-24 02:49:41 +0200

    chore: removed health check method

--- nigeria_geodata/config.py
+++ nigeria_geodata/config.py
@@ -53,20 +53,6 @@ class Config:
         return Config.DATA_SOURCES.get(source, {}).get("SERVICE_INFO_URL")
 
     @staticmethod
-    def get_health_check_url(source: DataSourceInfo) -> str:
-        """
-        Get the service URL for a given data source.
-
-        Args:
-            source (str): The name of the data source (e.g., 'GRID3').
-
-        Returns:
-            str: The service URL for the specified data source.
-        """
-
-        return Config.DATA_SOURCES.get(source, {}).get("HEALTH_CHECK_URL")
-
-    @staticmethod
     def get_data_source_info(source: DataSourceInfo) -> Dict[str, DataSourceInfo]:
         """
         Get detailed information about a given data source.

7e17b27b4170bf8e8d96a3e0a5b90fe91585a957
jeafreezy <jolaiyaemmanuel@gmail.com>
2024-08-24 09:58:15 +0200

    chore: updated versio to match pypi

--- nigeria_geodata/__init__.py
+++ nigeria_geodata/__init__.py
@@ -38,6 +38,6 @@ Usage:
 from nigeria_geodata.datasources.grid3 import Grid3, AsyncGrid3
 from nigeria_geodata.utils.geodata import NigeriaAdmin
 
-__version__ = "0.0.9"
+__version__ = "0.0.0"
 
 __all__ = ["Grid3", "AsyncGrid3", "NigeriaAdmin"]

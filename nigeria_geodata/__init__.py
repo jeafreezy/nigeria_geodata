60b689f73b926ab72ad7927fe4718929fe8b0b2d
jeafreezy <jolaiyaemmanuel@gmail.com>
2024-08-24 09:42:47 +0200

    chore:updated version

--- nigeria_geodata/__init__.py
+++ nigeria_geodata/__init__.py
@@ -38,6 +38,6 @@ Usage:
 from nigeria_geodata.datasources.grid3 import Grid3, AsyncGrid3
 from nigeria_geodata.utils.geodata import NigeriaAdmin
 
-__version__ = "0.0.1-beta"
+__version__ = "0.0.8"
 
 __all__ = ["Grid3", "AsyncGrid3", "NigeriaAdmin"]
